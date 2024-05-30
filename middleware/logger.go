e19aef1e5ca3d242f95da2fff7f0e7d79e4e8841
Joe Rozner <joe@deadbytes.net>
2024-05-29 23:14:24 -0700

    Add a slogger

--- middleware/logger.go
+++ middleware/logger.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"log"
+	"log/slog"
 	"net/http"
 	"time"
 
@@ -20,6 +21,25 @@ func Logger(l *log.Logger) func(next http.Handler) http.Handler {
 				next.ServeHTTP(wrapper, r)
 				end := time.Now()
 				l.Printf("%s %s: %s %d %dms\n", time.Now().Format(time.RFC3339), r.RemoteAddr, r.URL.Path, wrapper.Status(), end.Sub(start)/time.Millisecond)
+			}
+		}
+
+		return http.HandlerFunc(fn)
+	}
+}
+
+func SLogger(l *slog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			wrapper, ok := w.(*wrappers.ResponseWrapper)
+			if !ok {
+				l.Error("unable to assert ResponseWriter to ResponseWrapper")
+				next.ServeHTTP(w, r)
+			} else {
+				next.ServeHTTP(wrapper, r)
+				end := time.Now()
+				l.Info("", "status", wrapper.Status(), "remote", r.RemoteAddr, "path", r.URL.Path, "duration", end.Sub(start))
 			}
 		}
 

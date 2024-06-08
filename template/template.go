0264ea6ccd947888140721ee0a50bae29bd33a37
Joe Rozner <joe@deadbytes.net>
2024-06-07 21:29:33 -0700

    First pass at template helpers
    These wrap the standard go templating in order to provide some
    convenience and make it simpler to create layouts. This probably isn't
    flexible enough for production code, especially using HTMX, but it's a
    decent starting point.

---
+++ template/template.go
@@ -0,0 +1,75 @@
+package template
+
+import (
+	"errors"
+	"html/template"
+	"io"
+	"io/fs"
+	"os"
+	"path"
+	"strings"
+)
+
+var (
+	ErrTemplateExists = errors.New("template already exists")
+	ErrNoTemplate     = errors.New("template not found")
+)
+
+// GenerateTemplates produces a map of templates based on a common base layout. It expects the following directory
+// structure
+//
+// templates/contacts/index.html
+// templates/contacts/new.html
+// templates/names/index.html
+// templates/names/new.html
+//
+// The base directory name does not matter, but it must not include an underscore as the first character. The base
+// layouts should occur within a directory called _layout in the root templates directory
+func GenerateTemplates(templatesPath string) (map[string]*template.Template, error) {
+	templates := make(map[string]*template.Template)
+
+	base := template.Must(template.ParseGlob(path.Join(templatesPath, "_layouts", "*.html")))
+
+	err := fs.WalkDir(os.DirFS(templatesPath), ".", func(p string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+
+		if strings.HasPrefix(p, "_") {
+			return nil
+		}
+
+		if _, ok := templates[p]; ok {
+			// this shouldn't be possible because the naming it based on file paths, but we should handle it if it does
+			return ErrTemplateExists
+		}
+
+		cloned, err := base.Clone()
+		if err != nil {
+			return err
+		}
+
+		t, err := cloned.ParseFiles(path.Join(templatesPath, p))
+		if err != nil {
+			return err
+		}
+
+		templates[p] = t
+
+		return nil
+	})
+
+	return templates, err
+}
+
+// Render finds the specified template in the map of templates, executes it, and writes it to the writer.
+func Render(w io.Writer, templates map[string]*template.Template, name string, data interface{}) error {
+	t, ok := templates[name]
+	if !ok {
+		return ErrNoTemplate
+	}
+
+	name = path.Base(name)
+
+	return t.ExecuteTemplate(w, name, data)
+}
